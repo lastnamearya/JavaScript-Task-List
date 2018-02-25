9c637b1cbdbeef6427882c9ae505ed60eca0b61d
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-26 02:35:22 +0530

    convert object into an array

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+// Converting an Object into an Array
 
+let scripts = document.scripts;
+
+let scriptsArr = Array.from(scripts);
+
+scriptsArr.forEach(function(script) {
+  console.log(script.getAttribute('src'));
+});
