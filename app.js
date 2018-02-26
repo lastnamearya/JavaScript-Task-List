b8016dad5be3331f9d1d30517fe788c27b635505
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-26 17:27:03 +0530

    add & remove classes

--- app.js
+++ app.js
@@ -15,4 +15,29 @@ const cardAction = document.querySelector('.card-action');
 // Replace
 cardAction.replaceChild(newHeading, oldHeading);
 
-console.log(newHeading);
+// Remove Element
+const lis = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+// Remove list item
+lis[0].remove();
+
+// Remove child element
+list.removeChild(lis[3]);
+
+// Classes and Attributes
+const firstLi = document.querySelector('li:first-child');
+// Note: Always use an index bracket, because children will give us an collection of elements
+const link = firstLi.children[0];
+
+let val;
+
+val = link.className;
+// It will give us a DOM Token List
+val = link.classList;
+val = link.classList[0];
+link.classList.add('test');
+link.classList.remove('test');
+val = link;
+
+console.log(val);
