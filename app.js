522ee26a65209b1e65e42be467265a2ee51b7aaa
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-26 15:03:34 +0530

    local scope selection

--- app.js
+++ app.js
@@ -1,32 +1,13 @@
-// // document.getElementById();
+// document.getElementsByClassName
 
-// console.log(document.getElementById('task-title'));
+// IT'll select items on the behalf of Global Scope.
+const items = document.getElementsByClassName('collection-item');
+console.log(items);
+console.log(items[0]);
+items[0].style.color = "red";
+items[3].textContent = "Hello";
 
-// // Get things from the element
-// console.log(document.getElementById('task-title').id);
-// console.log(document.getElementById('task-title').className);
+// This selection is limited to Local Scope Only
+const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
 
-// const taskTitle = document.getElementById('task-title');
-
-// // Change Styling
-// taskTitle.style.background = "#333";
-// taskTitle.style.color = "#fff";
-// taskTitle.style.padding = "5px";
-// // document.getElementById('task-title').style.display = "none";
-
-// // change content
-// taskTitle.textContent = "Task List";
-// taskTitle.innerText = "My Tasks";
-// taskTitle.innerHTML = "<span style='color:red'>Task List</span>";
-
-// document.querySelector() 
-console.log(document.querySelector('#task-title'));
-console.log(document.querySelector('.card-title'));
-console.log(document.querySelector('h5')); 
-
-document.querySelector('ul li').style.color = "blue";
-document.querySelector('li:last-child').style.color = "red";
-document.querySelector('li:nth-child(3)').style.color = "yellow";
-document.querySelector('li:nth-child(4)').textContent = "Hello World";
-document.querySelector('li:nth-child(odd)').style.background = "#ccc";
-document.querySelector('li:nth-child(even)').style.background = "#f4f4f4";
+console.log(listItems);
