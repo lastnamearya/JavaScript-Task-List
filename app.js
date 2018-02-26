c88cc15cc525cf151431c0b84aa059aa158510bd
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-26 15:16:31 +0530

    getElementsByTagName & forEach

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-// document.getElementsByClassName
+// // document.getElementsByClassName
 
-// IT'll select items on the behalf of Global Scope.
-const items = document.getElementsByClassName('collection-item');
-console.log(items);
-console.log(items[0]);
-items[0].style.color = "red";
-items[3].textContent = "Hello";
+// // IT'll select items on the behalf of Global Scope.
+// const items = document.getElementsByClassName('collection-item');
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = "red";
+// items[3].textContent = "Hello";
 
-// This selection is limited to Local Scope Only
-const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+// // This selection is limited to Local Scope Only
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
 
-console.log(listItems);
+// console.log(listItems);
+
+// document.getElementsByTagName
+let lis = document.getElementsByTagName('li');
+console.log(lis);
+console.log(lis[0]);
+lis[0].style.color = "red";
+lis[3].textContent = "Hello";
+
+// Convert HTML Collection (an object) into an array
+lis = Array.from(lis);
+
+lis.reverse();
+
+lis.forEach(function(li, index){
+  console.log(li.className);
+  li.textContent = `${index}: Hello`;
+});
+
+console.log(lis);
