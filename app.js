b1ac964f1b790dfe909fd0f66566f7fe254b898a
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-26 16:21:07 +0530

    parentNode & parentElement

--- app.js
+++ app.js
@@ -22,8 +22,28 @@ val = list.childNodes[1].nodeType;
 // 10 - Doctype
 
 // Get Children element Nodes
-// val = list.children;
+val = list.children;
+val = list.children[1];
+list.children[1].textContent = 'Hello';
 
+// Children of Children
+list.children[3].children[0].id = 'test-link';
+val = list.children[3].children[0];
 
+// First child
+val = list.firstChild;
+val = list.firstElementChild;
+
+// Last child
+val = list.lastChild;
+val = list.lastElementChild;
+
+// Count child Elements
+val = list.childElementCount;
+
+// Get Parent Node
+val = listItem.parentNode;
+val = listItem.parentElement;
+val = listItem.parentElement.parentElement;
 
 console.log(val);
