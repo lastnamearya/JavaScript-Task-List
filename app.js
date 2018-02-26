d06692f2457487a6212af0534d88ebc5cbcb1aeb
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-26 17:38:23 +0530

    attribute & methods

--- app.js
+++ app.js
@@ -32,12 +32,22 @@ const link = firstLi.children[0];
 
 let val;
 
+// Classes
 val = link.className;
 // It will give us a DOM Token List
 val = link.classList;
 val = link.classList[0];
 link.classList.add('test');
 link.classList.remove('test');
+val = link;
+
+// Attributes
+val = link.getAttribute('href');
+val = link.setAttribute('href', 'http://google.com');
+val = link.hasAttribute('href');
+link.setAttribute('title', 'Google');
+val = link.hasAttribute('title');
+link.removeAttribute('title');
 val = link;
 
 console.log(val);
