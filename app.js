1c0ac90e329767351b16fdee432683e3651f7061
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-27 00:38:52 +0530

    form error debugged

--- app.js
+++ app.js
@@ -4,3 +4,21 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+// Load all event Listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // Add task Event
+  form.addEventListener('submit', addTask);
+}
+
+// Add Task
+function addTask(e){
+  if(taskInput.value === ''){
+    alert("Add a task");
+  }
+
+  e.preventDefault();
+}
