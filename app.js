e6ab72b34b919ad6fec37fde786fd7c5fac2058a
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-26 16:06:43 +0530

    childNodes & Types

--- app.js
+++ app.js
@@ -1,53 +1,29 @@
-// // document.getElementsByClassName
+let val;
 
-// // IT'll select items on the behalf of Global Scope.
-// const items = document.getElementsByClassName('collection-item');
-// console.log(items);
-// console.log(items[0]);
-// items[0].style.color = "red";
-// items[3].textContent = "Hello";
+// This will going to the first ul element with the class of 'collection' but with the element we also get its child that are all list items under the parent 'ul'
+const list = document.querySelector('ul.collection');
+// This will going to select the first list item
+const listItem = document.querySelector('li.collection-item:first-child');
 
-// // This selection is limited to Local Scope Only
-// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+val = listItem;
+val = list;
 
-// console.log(listItems);
+// Get Child Nodes
+val = list.childNodes;
+val = list.childNodes[0];
+val = list.childNodes[0].nodeName;
+val = list.childNodes[1].nodeType;
 
-// // document.getElementsByTagName
-// let lis = document.getElementsByTagName('li');
-// console.log(lis);
-// console.log(lis[0]);
-// lis[0].style.color = "red";
-// lis[3].textContent = "Hello";
+// 1 - Element
+// 2 - Attribute (deprecated)
+// 3 - Text Node
+// 8 - Comment
+// 9 - Document itself
+// 10 - Doctype
 
-// // Convert HTML Collection (an object) into an array
-// lis = Array.from(lis);
+// Get Children element Nodes
+// val = list.children;
 
-// lis.reverse();
 
-// lis.forEach(function(li, index){
-//   console.log(li.className);
-//   li.textContent = `${index}: Hello`;
-// });
 
-// console.log(lis);
-
-// document.querySelectorAll()
-const items = document.querySelectorAll('ul.collection li.collection-item');
-
-items.forEach(function(item, index){
-  item.textContent = `${index}: Hello`;
-});
-
-const liOdd = document.querySelectorAll('li:nth-child(odd)');
-const liEven = document.querySelectorAll('li:nth-child(even)');
-
-liOdd.forEach(function(li, index){
-  li.style.background = '#ccc';
-});
-
-// for loop will work for HTML Collection, if we don't convert it into an array
-for(let i = 0; i < liEven.length; i++){
-  liEven[i].style.background = '#f4f4f4';
-}
-
-console.log(items);
+console.log(val);
