7ee25c07891061d76ff866f4757e60bcdd39ca01
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-27 01:50:09 +0530

    Now we can filter Tasks

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ function loadEventListeners() {
   taskList.addEventListener('click', removeTask);
   // Clear Task Events
   clearBtn.addEventListener('click', clearTasks);
+  // Filter Tasks events
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -69,4 +71,18 @@ function clearTasks() {
   while(taskList.firstChild){
     taskList.removeChild(taskList.firstChild);
   }
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase(); 
+
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none'; 
+    }
+  });
 }
