4fd90ba011514f7b974d7858a39c2844dc65427f
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-26 17:55:57 +0530

    prevent Default Behaviour

--- app.js
+++ app.js
@@ -1,53 +1,5 @@
-// Replace Element
+document.querySelector('.clear-tasks').addEventListener('click', (e) => {
+  console.log('Hello World');
 
-// Create Element
-const newHeading = document.createElement('h2');
-// Add id
-newHeading.id = "task-title";
-// Add text Node
-newHeading.appendChild(document.createTextNode('Task List'));
-
-// Get the old heading
-const oldHeading = document.getElementById('task-title');
-// Parent
-const cardAction = document.querySelector('.card-action');
-
-// Replace
-cardAction.replaceChild(newHeading, oldHeading);
-
-// Remove Element
-const lis = document.querySelectorAll('li');
-const list = document.querySelector('ul');
-
-// Remove list item
-lis[0].remove();
-
-// Remove child element
-list.removeChild(lis[3]);
-
-// Classes and Attributes
-const firstLi = document.querySelector('li:first-child');
-// Note: Always use an index bracket, because children will give us an collection of elements
-const link = firstLi.children[0];
-
-let val;
-
-// Classes
-val = link.className;
-// It will give us a DOM Token List
-val = link.classList;
-val = link.classList[0];
-link.classList.add('test');
-link.classList.remove('test');
-val = link;
-
-// Attributes
-val = link.getAttribute('href');
-val = link.setAttribute('href', 'http://google.com');
-val = link.hasAttribute('href');
-link.setAttribute('title', 'Google');
-val = link.hasAttribute('title');
-link.removeAttribute('title');
-val = link;
-
-console.log(val);
+  e.preventDefault();
+});
